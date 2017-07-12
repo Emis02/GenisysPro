5f65ce3bbcd62d49b0445694e57fd338049fdece
SuperKali <danychannel18@gmail.com>
2017-07-12 17:36:23 +0200

    Fix...

--- src/pocketmine/block/ItemFrame.php
+++ src/pocketmine/block/ItemFrame.php
@@ -2,24 +2,22 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *  _____   _____   __   _   _   _____  __    __  _____
+ * /  ___| | ____| |  \ | | | | /  ___/ \ \  / / /  ___/
+ * | |     | |__   |   \| | | | | |___   \ \/ /  | |___
+ * | |  _  |  __|  | |\   | | | \___  \   \  /   \___  \
+ * | |_| | | |___  | | \  | | |  ___| |   / /     ___| |
+ * \_____/ |_____| |_|  \_| |_| /_____/  /_/     /_____/
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
+ * @author iTX Technologies
+ * @link https://itxtech.org
  *
- *
-*/
-
-declare(strict_types=1);
+ */
 
 namespace pocketmine\block;
 
@@ -35,7 +33,7 @@ use pocketmine\tile\Tile;
 class ItemFrame extends Flowable{
 	protected $id = Block::ITEM_FRAME_BLOCK;
 
-	public function __construct(int $meta = 0){
+	public function __construct($meta = 0){
 		$this->meta = $meta;
 	}
 
@@ -43,9 +41,12 @@ class ItemFrame extends Flowable{
 		return "Item Frame";
 	}
 
-	public function onActivate(Item $item, Player $player = null) : bool{
-		$tile = $this->level->getTile($this);
-		if(!($tile instanceof TileItemFrame)){
+	public function canBeActivated() : bool{
+		return true;
+	}
+
+	public function onActivate(Item $item, Player $player = null){
+		if(!(($tile = $this->level->getTile($this)) instanceof TileItemFrame)){
 			$nbt = new CompoundTag("", [
 				new StringTag("id", Tile::ITEM_FRAME),
 				new IntTag("x", $this->x),
@@ -60,14 +61,35 @@ class ItemFrame extends Flowable{
 		if($tile->hasItem()){
 			return;//anyone can rotate items in itemframe #to fix
 			$tile->setItemRotation(($tile->getItemRotation() + 1) % 8);
-		}elseif($item->getCount() > 0){
-			$tile->setItem($item->pop());
+		}else{
+			if($item->getCount() > 0){
+				$frameItem = clone $item;
+				$frameItem->setCount(1);
+				$item->setCount($item->getCount() - 1);
+				$tile->setItem($frameItem);
+				if($item->getId() === Item::FILLED_MAP){
+					$tile->SetMapID($item->getMapId());
+				}
+				if($player instanceof Player and $player->isSurvival()){
+					$player->getInventory()->setItemInHand($item->getCount() <= 0 ? Item::get(Item::AIR) : $item);
+				}
+			}
 		}
 
 		return true;
 	}
 
-	public function onUpdate(int $type){
+	public function onBreak(Item $item){
+		if(($tile = $this->level->getTile($this)) instanceof TileItemFrame){
+			//TODO: add events
+			if(lcg_value() <= $tile->getItemDropChance() and $tile->getItem()->getId() !== Item::AIR){
+				$this->level->dropItem($tile->getBlock(), $tile->getItem());
+			}
+		}
+		return parent::onBreak($item);
+	}
+
+	public function onUpdate($type){
 		if($type === Level::BLOCK_UPDATE_NORMAL){
 			$sides = [
 				0 => 4,
@@ -83,7 +105,7 @@ class ItemFrame extends Flowable{
 		return false;
 	}
 
-	public function place(Item $item, Block $block, Block $target, int $face, float $fx, float $fy, float $fz, Player $player = null) : bool{
+	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
 		if($face === 0 or $face === 1){
 			return false;
 		}
@@ -120,18 +142,9 @@ class ItemFrame extends Flowable{
 	}
 
 	public function getDrops(Item $item) : array{
-		$drops = [
-			Item::get(Item::ITEM_FRAME, 0, 1)
+		return [
+			[Item::ITEM_FRAME, 0, 1]
 		];
-
-		$tile = $this->level->getTile($this);
-		if($tile instanceof TileItemFrame){
-			if(lcg_value() <= $tile->getItemDropChance() and $tile->hasItem()){
-				$drops[] = $tile->getItem();
-			}
-		}
-
-		return $drops;
 	}
 
 }
